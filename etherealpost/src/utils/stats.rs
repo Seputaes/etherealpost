1b25e83795f1ee4cddf4921664343aad01288759
Seputaes <seputaes@sep.gg>
2021-04-04 13:49:48 -0700

    Add ItemLevelCurve for calculating effective item levels

--- etherealpost/src/utils/stats.rs
+++ etherealpost/src/utils/stats.rs
@@ -2,6 +2,150 @@
 const MINIMUM_PRICES_PERCENTILE: f64 = 15.0;
 const FIRST_STANDARD_DEV_PERCENTILE: f64 = 15.87;
 
+/// An individual curve point, which represents `(x, y)` coordinates on a graph,
+/// where `x` is player level and `y` is the effective level of the item.
+struct CurvePoint {
+    pub player_level: f64,
+    pub item_level: f64,
+}
+
+/// Item Level Curve, which loosely corresponds to the
+/// `CurvePoint` DB2 table in World of Warcraft, can be used to determine
+/// the effective item level of an item based on the level the player was
+/// when they looted it by applying
+/// <a href="https://en.wikipedia.org/wiki/Linear_interpolation" target="_blank">linear interpolation</a>.
+///
+/// An Item Level Curve consists of a vector of `(u32, u32)` data points which
+/// represent `(x, y)` coordinates on a graph, where `x` is player level and
+/// `y` is the effective item level of the item. Using interpolation,
+/// on this data set, we can determine what `y` should be for any given `x`
+/// which in this case is the player's level when the item dropped.
+///
+/// In the [Item](`crate::battlenet::auctions::Item`) on an Auction, the player's level
+/// when the item dropped can be found in the
+/// [modifiers](`crate::battlenet::auctions::Item::modifiers`) field for the modifier
+/// with a type of `9`. If this is not set, the item level falls back on the base
+/// level of the item.
+///
+/// See [`ItemLevelCurve::calc_ilvl`] for a detailed explanation of the formula used.
+pub struct ItemLevelCurve {
+    /// The curve points (`(x,y)` coordinates) which are associated with this item
+    /// level curve.
+    points: Vec<CurvePoint>,
+}
+
+impl ItemLevelCurve {
+    /// Create a new Item Level curve from a vector of `(x, y)` coordinates.
+    /// These values will be cloned into the resulting struct. As such,
+    /// they do not need to be mutable or previously sorted.
+    ///
+    /// The curve points can be found for a given _curve ID_ in the `CurvePoint`
+    /// DB2 table in World of Warcraft.
+    pub fn from_points(points: &Vec<(u32, u32)>) -> ItemLevelCurve {
+        let mut curve = ItemLevelCurve {
+            points: points
+                .iter()
+                .map(|(x, y)| CurvePoint {
+                    player_level: *x as f64,
+                    item_level: *y as f64,
+                })
+                .collect(),
+        };
+        curve
+            .points
+            .sort_by(|a, b| a.player_level.partial_cmp(&b.player_level).unwrap());
+        curve
+    }
+
+    /// Using the item level curve points, calculates the effective item level
+    /// based on the player's level when the item was looted.
+    ///
+    /// # Formula
+    ///
+    /// The equation used is <a href="https://en.wikipedia.org/wiki/Linear_interpolation" target="_blank">linear interpolation</a>.
+    ///
+    /// ```text
+    /// y = y₀ + (x - x₀) * ((y₁ - y₀) / (x₁ - x₀))
+    /// ```
+    ///
+    /// * `y` = effective item level
+    /// * `x` = looted item level
+    /// * `x₀` = player item level in the curve at position `n - 1`
+    /// * `x₁` = player item level in the curve at position `n`
+    /// * `y₀` = item level in the curve at position `n - 1`
+    /// * `y₁` = item level in the curve at position `n`
+    /// * `n` = position in the curve series where `x₀ > x <= x₁`
+    ///
+    /// Let's use the following curve points as an example. This corresponds
+    /// to curve ID `1748` (as of 9.0.5).
+    ///
+    /// ```text
+    /// [
+    ///   (1, 6), (25, 31), (26, 32), (27, 33), (28, 34),
+    ///   (31, 38), (32, 39), (39, 46), (40, 47), (42, 50),
+    ///   (43, 50), (44, 50), (45, 50), (46, 51), (49, 57),
+    ///   (50, 57), (51, 98), (59, 146), (60, 146)
+    /// ]
+    /// ```
+    ///
+    /// If a player looted an item with this curve ID at level `37`, the formula would be:
+    ///
+    /// ```text
+    /// y = 39 + (37 - 32) * ((46 - 39) / (39 - 32))
+    /// y = 44 (effective item level)
+    /// ```
+    ///
+    /// # Special Conditions
+    ///
+    /// The interpolation formula does not strictly apply in the following situations:
+    ///
+    /// * Where `x` equals any `xₙ` in the curve. In this case, `y = yₙ`.
+    /// * Where `x` is greater than _all_ `xₙ` in the curve. In this case, `y = yₗ` where `yₗ`
+    ///   is the last item in the curve series.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use etherealpost::stats::ItemLevelCurve;
+    ///
+    /// let curve_points: Vec<(u32, u32)> = vec![
+    ///     (1, 6), (25, 31), (26, 32), (27, 33), (28, 34),
+    ///     (31, 38), (32, 39), (39, 46), (40, 47), (42, 50),
+    ///     (43, 50), (44, 50), (45, 50), (46, 51), (49, 57),
+    ///     (50, 57), (51, 98), (59, 146), (60, 146)
+    ///  ];
+    ///
+    /// let ilvl_curve = ItemLevelCurve::from_points(&curve_points);
+    /// assert_eq!(12, ilvl_curve.calc_ilvl(&7));
+    /// assert_eq!(44, ilvl_curve.calc_ilvl(&37));
+    /// assert_eq!(104, ilvl_curve.calc_ilvl(&52));
+    /// assert_eq!(146, ilvl_curve.calc_ilvl(&65));
+    /// ```
+    pub fn calc_ilvl(&self, looted_level: &u32) -> u32 {
+        let looted_level = *looted_level as f64;
+
+        let mut prev = &self.points[0];
+
+        for point in &mut self.points.iter() {
+            // if the player level is the looted level, we don't need to interpolate
+            if looted_level == point.player_level {
+                return point.item_level as u32;
+            }
+            // interpolate: y = y0 + (x - x0) * ( (y1 - y0) / (x1 - x0) )
+            if looted_level < point.player_level {
+                return (prev.item_level
+                    + (looted_level - prev.player_level)
+                        * ((point.item_level - prev.item_level)
+                            / (point.player_level - prev.player_level)))
+                    as u32;
+            }
+            prev = point;
+        }
+
+        prev.item_level as u32
+    }
+}
+
 /// Calculates the Market Price given an array of item buyout our unit prices.
 ///
 /// In the context of a realm, "Market Price" is defined as the bottom "1 standard
@@ -456,5 +600,36 @@ mod tests {
     fn normalized_market_price_single_item() {
         let res = normalized_market_price(&mut [10000]).unwrap();
         assert_eq!(10000, res);
+    }
+
+    #[test]
+    fn calc_ilvl_looted_level_in_curve() {
+        let curve_points: Vec<(u32, u32)> = vec![(1, 6), (25, 31), (26, 32), (27, 33)];
+        let curve = ItemLevelCurve::from_points(&curve_points);
+        assert_eq!(32, curve.calc_ilvl(&26));
+    }
+
+    #[test]
+    fn calc_ilvl_curve_with_zero_level() {
+        let curve_points: Vec<(u32, u32)> = vec![(0, 99)]; // Curve ID 19995
+        let curve = ItemLevelCurve::from_points(&curve_points);
+        assert_eq!(99, curve.calc_ilvl(&1));
+        assert_eq!(99, curve.calc_ilvl(&25));
+        assert_eq!(99, curve.calc_ilvl(&50));
+        assert_eq!(99, curve.calc_ilvl(&60));
+    }
+
+    #[test]
+    fn calc_ilvl_curve_looted_level_gt_max_curve() {
+        let curve_points: Vec<(u32, u32)> = vec![(1, 6), (25, 31), (26, 32), (27, 33)];
+        let curve = ItemLevelCurve::from_points(&curve_points);
+        assert_eq!(33, curve.calc_ilvl(&60));
+    }
+
+    #[test]
+    fn calc_ilvl_curve_looted_level_lt_one_curve() {
+        let curve_points: Vec<(u32, u32)> = vec![(1, 6), (25, 31), (26, 32), (27, 33)];
+        let curve = ItemLevelCurve::from_points(&curve_points);
+        assert_eq!(12, curve.calc_ilvl(&7));
     }
 }
